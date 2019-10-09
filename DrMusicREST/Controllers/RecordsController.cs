636a9fd22445c1de2e6d7eedeb475c69afd4a5c4
danielegtved <43374944+danielegtved@users.noreply.github.com>
2019-10-09 09:35:30 +0200

    Add project files.

---
+++ DrMusicREST/Controllers/RecordsController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DrMusicREST.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DrMusicREST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecordsController : ControllerBase
+    {
+        public static readonly List<Record> records = new List<Record>
+        {
+            new Record("Hej", "Knud", new TimeSpan(0,3,16), new DateTime(2001, 2, 1)),
+            new Record("Hallo", "Bent", new TimeSpan(0,4,41), new DateTime(2002, 3, 1)),
+            new Record("Lucas Hjælp", "Morten", new TimeSpan(0,1,20), new DateTime(2004, 4, 1)),
+            new Record("Fuck dig Morten", "Lucas", new TimeSpan(0,5,40), new DateTime(2004, 1, 1))
+        };
+        // GET: api/Records
+        [HttpGet]
+        public IEnumerable<Record> Get()
+        {
+            return records;
+        }
+
+        // GET: api/Records/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Records
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Records/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DrMusicREST/Model/Record.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DrMusicREST.Model
+{
+    public class Record
+    {
+        public Record(string title, string artist, TimeSpan duration, DateTime dateOfPublication)
+        {
+            Title = title;
+            Artist = artist;
+            Duration = duration;
+            DateOfPublication = dateOfPublication;
+        }
+
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime DateOfPublication { get; set; }
+
+    }
+}
